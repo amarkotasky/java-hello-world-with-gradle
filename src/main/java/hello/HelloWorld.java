e6fb67c77e5d475af03817213df6b86d2df3d6e7
amarkotasky <amarkotasky@gmail.com>
2018-12-08 22:20:17 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is 123: " + currentTime);
+    System.out.println("The current 123 local time is 123: " + currentTime);
     
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
